19b8514865a184f69c284001eafa2ce9b2dd6b56
prafulpawar <prafuldukhi@gmail.com>
2024-12-30 17:47:53 +0530

    Added OnOffBulb Project

--- bulb/src/App.jsx
+++ bulb/src/App.jsx
@@ -1,15 +1,63 @@
-import React from 'react'
-React.createElement('h1',{props})
+
+// //Ye Jo Example Hai vo Conditial Rendering Aur Normal Function Ke Sath Me Hai 
+
+// import React, { useState } from 'react'
+
+// function App() {
+//   const [value,setvalue]= useState(false)
+
+//   function handleLight (){
+      
+//           setvalue(!value)
+      
+      
+//   }
+
+//   return (
+   
+//         <>
+//             <div className='bg-zinc-300 h-screen flex justify-center items-center flex-col'>
+//                     <div className='border-2 rounded-full w-24 h-24 '>
+//                         <div className={` ${value ? 'w-full h-full bg-red-400 rounded-full' : 'w-full h-full bg-white rounded-full'}`}  >
+
+//                         </div>
+//                     </div> 
+
+//                     <div className='rounded-md border-2 mt-2 pr-6 pl-6 pt-1 pb-1' onClick={handleLight}>
+//                          <button>Button</button>
+//                     </div>
+//             </div>
+//         </>      
+//   )
+// }
+
+// export default App
+
+//Ye Jo Example Hai vo Conditial Rendering Aur Arrow Function Ke Sath Me Hai 
+// Jo Ki Time Par Direct useState Ki Value ko Chnage Karenga 
+
+
+import React, { useState } from 'react'
+
 function App() {
+  const [value,setvalue]= useState(false)
+
+
   return (
-      
-      
+   
         <>
-           <h1>jasbc</h1>
-           <h1>kbscji</h1>
-        </>
-      
-      
+            <div className='bg-zinc-300 h-screen flex justify-center items-center flex-col'>
+                    <div className='border-2 rounded-full w-24 h-24 '>
+                        <div className={` ${value ? 'w-full h-full bg-red-400 rounded-full' : 'w-full h-full bg-white rounded-full'}`}  >
+
+                        </div>
+                    </div> 
+
+                    <div className='rounded-md border-2 mt-2 pr-6 pl-6 pt-1 pb-1' onClick={()=>setvalue(!value)}>
+                         <button>Button</button>
+                    </div>
+            </div>
+        </>      
   )
 }
 
