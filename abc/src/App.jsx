275ee08870f8661f5d25330ff09a5614bad6769a
prafulpawar <prafuldukhi@gmail.com>
2024-12-30 19:11:34 +0530

    Added Rendering

--- abc/src/App.jsx
+++ abc/src/App.jsx
@@ -1,6 +1,50 @@
+import React from 'react'
+
 function App() {
+  // let a = 'hello world'
+  // let b = 12 ///Number Data Types Then--->Int We Can Say 
+  // let c = true
+  // let d = null
+  // let e = undefined
+  // let f = [1,2,3,4,5] //Primitive Data Types Of Arr
+  // let g = ['name',5,()=>abc]
+  // let h ={ name:"Abc", age:'25' }
+
+  // let profiles = [
+
+  //   {
+  //     name: "ABC",
+  //     age: 251
+  //   },
+
+  //   {
+  //     name: "BCD",
+  //     age: 780
+  //   },
+
+  //   {
+  //     name: "XYZ",
+  //     age: 250
+  //   },
+  // ]
+
+  // {
+  //   profiles.map((elem, index) => {
+  //     return (
+  //       <div key={index}>
+  //         <h1>{elem.name}</h1>
+  //         <p>{elem.age}</p>
+  //       </div>
+  //     )
+  //   })
+  // }
+
   return (
-    <div>App</div>
+    <>
+     
+    </>
   )
+
 }
+
 export default App
--- frndreq/src/App.jsx
+++ frndreq/src/App.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
 
 function App() {
+  let a = 'hello world'
+  let b = 12 ///Number Data Types Then--->Int We Can Say 
+  let c = true
+  let d = null
+  let e = undefined
+  let f = [1,2,3,4,5] //Primitive Data Types Of Arr
+  let g = ['name',{},5]
+  let h ={ name:"Abc", age:25 }
   return (
     <div>
-      
+          {
+            a
+          }
+          {
+            b
+          }{
+            c
+          }
+          {
+            d
+          }{
+            e
+          }
+          {
+            f
+          }
+          {
+            h
+          }
+          {
+            g
+          }
     </div>
   )
 }
