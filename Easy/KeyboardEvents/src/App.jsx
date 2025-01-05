8393e161eb6ad2ac803e1eb7de76d86b92d666b5
prafulpawar <prafuldukhi@gmail.com>
2025-01-05 14:10:11 +0530

    "Done"

--- Easy/KeyboardEvents/src/App.jsx
+++ Easy/KeyboardEvents/src/App.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
-import KeyboardEventExample from './compoents/Ktrack'
+// import KeyboardEventExample from './compoents/Ktrack'
 function App() {
+  const GlobalKeyboardEvent = () => {
+    const [key, setKey] = useState('');
+  
+    useEffect(() => {
+      const handleKeyDown = (e) => {
+        setKey(e.key);
+      };
+  
+      window.addEventListener('keydown', handleKeyDown);
+  
+      // Cleanup listener on unmount
+      return () => {
+        window.removeEventListener('keydown', handleKeyDown);
+      };
+    }, []);
   return (
-     <KeyboardEventExample/>
+    //  <KeyboardEventExample/>
+        <div onKeyDown={GlobalKeyboardEvent} className="h-screen flex justify-center items-center">
+          <h1>Key Pressed: {key}</h1>
+        </div>
   )
+}
 }
 
 export default App
